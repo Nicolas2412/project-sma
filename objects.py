a19ddca565d9cf5cb117909868de4c82ec3f55ea
Nicolas Charrondiere <nicolas.charrondiere@student-cs.fr>
2026-03-16 14:04:06 +0100

    added headers to files

---
+++ objects.py
@@ -0,0 +1,10 @@
+################################################################################
+# PROJET : SMA (Systees Multi-Agents) - Groupe 6
+# DATE DE CRÉATION : 16/03/2026
+#
+# MEMBRES DU GROUPE :
+#   - Nicolas Charronidere
+#   - Paul Guimbert
+#
+# FILE: objects.py
+################################################################################
