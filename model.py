c6fa2044a0035eabdc2791c661420cd732d6c94e
Nicolas Charrondiere <nicolas.charrondiere@student-cs.fr>
2026-03-16 16:23:11 +0100

    working vizualisation

--- model.py
+++ model.py
@@ -8,3 +8,94 @@
 #
 # FILE: model.py
 ################################################################################
+
+import mesa
+from agents import *
+from objects import *
+
+class RobotModel(mesa.Model):
+    def __init__(self,
+                n_green_agents:int,
+                n_yellow_agents:int,
+                n_red_agents:int,
+                n_green_wastes: int,
+                n_yellow_wastes: int,
+                n_red_wastes: int,
+                height:int,
+                width_z1:int,
+                width_z2:int,
+                width_z3:int,
+                seed = None):
+        """Creates a new RobotModel
+
+        Args:
+            seed (_type_, optional): Seed for the randoms. Defaults to None.
+        """
+        super().__init__(seed=seed)
+        
+        self.n_green = n_green_agents
+        self.n_yellow = n_yellow_agents
+        self.n_red = n_red_agents
+        self.n_green_wastes_wastes= n_green_wastes
+        self.n_yellow_wastes = n_yellow_wastes
+        self.n_red_wastes = n_red_wastes
+        
+        self.height = height
+        
+        self.width_z1 = width_z1
+        self.width_z2 = width_z2
+        self.width_z3 = width_z3
+        self.total_width = width_z1 + width_z2 + width_z3
+
+        self.grid = mesa.space.MultiGrid(self.total_width, height, False)
+        
+        for i in range(self.width_z1):
+            for j in range(self.height):
+                agent = Radioactivity(self, zone=1)
+                self.grid.place_agent(agent, (i, j))
+                
+        for i in range(self.width_z1):
+            for j in range(self.height):
+                agent = Radioactivity(self, zone=2)
+                self.grid.place_agent(agent, (self.width_z1 + i, j))
+                
+        for i in range(self.width_z1):
+            for j in range(self.height):
+                agent = Radioactivity(self, zone=3)
+                self.grid.place_agent(
+                    agent, (self.width_z1 + self.width_z2 + i, j))
+        
+        green_wastes = Waste.create_agents(model=self, n=n_green_wastes, color=1)
+        # Create x and y positions for agents
+        x = self.rng.integers(0, self.width_z1, size=(n_green_wastes,))
+        y = self.rng.integers(0, self.grid.height, size=(n_green_wastes,))
+        for a, i, j in zip(green_wastes, x, y):
+            # Add the agent to a random grid cell
+            self.grid.place_agent(a, (i, j))
+            
+        yellow_wastes = Waste.create_agents(
+            model=self, n=n_yellow_wastes, color=2)
+        # Create x and y positions for agents
+        x = self.rng.integers(
+            self.width_z1, self.width_z1 + self.width_z2, size=(n_yellow_wastes,))
+        y = self.rng.integers(0, self.grid.height, size=(n_yellow_wastes,))
+        for a, i, j in zip(yellow_wastes, x, y):
+            # Add the agent to a random grid cell
+            self.grid.place_agent(a, (i, j))
+
+        red_wastes = Waste.create_agents(
+            model=self, n=n_red_wastes, color=3)
+        # Create x and y positions for agents
+        x = self.rng.integers(self.width_z1 + self.width_z2,self.width_z1 + self.width_z2 + self.width_z3, size=(n_red_wastes,))
+        y = self.rng.integers(0, self.grid.height, size=(n_red_wastes,))
+        for a, i, j in zip(red_wastes, x, y):
+            # Add the agent to a random grid cell
+            self.grid.place_agent(a, (i, j))
+    # def do(self, agent:mesa.Agent, action:str):
+        
+        # if action == "move_up":
+        #     new_pos = (agent.pos[0], agent.pos[1] - 1)
+        #     if self.grid.is_cell_empty(new_pos):
+        #         self.model.grid.move_agent(agent, new_pos)
+        #     else:
+        #         pass
--- server.py
+++ server.py
@@ -8,3 +8,76 @@
 #
 # FILE: server.py
 ################################################################################
+
+import mesa
+import solara
+from agents import *
+from objects import *
+from model import RobotModel
+from mesa.visualization import SolaraViz, make_plot_component, make_space_component
+
+model_params = {
+    "n_green_agents" : 2,
+    "n_yellow_agents" : 2,
+    "n_red_agents" : 2,
+    "n_green_wastes" : 2,
+    "n_yellow_wastes" : 2,
+    "n_red_wastes" : 2,
+    "height" : 10,
+    "width_z1" : 10,
+    "width_z2" : 10,
+    "width_z3" : 10,
+    }
+
+def configure_axes(ax):
+    ax.set_aspect('equal', adjustable='box') # Force le ratio 1:1
+    ax.axis('off')                            # Supprime les numéros (0, 5, 10...)
+    return ax
+
+def agent_portrayal(agent):
+    # Dictionnaire avec des clés standard Matplotlib
+    portrayal = {}
+
+    if isinstance(agent, Radioactivity):
+        # On définit la couleur selon la zone
+        portrayal["marker"] = "s"
+        portrayal["size"] = 500
+        portrayal["zorder"] = 0
+
+        if agent.zone == 1:
+            portrayal["color"] = "#73ff78"  # Vert clair
+        elif agent.zone == 2:
+            portrayal["color"] = "#ffe482"  # Jaune clair
+        elif agent.zone == 3:
+            portrayal["color"] = "#f59188"  # Rouge clair
+            
+    elif isinstance(agent, Waste):
+        # On définit la couleur selon la zone
+        portrayal["marker"] = "o"
+        portrayal["size"] = 10
+        portrayal["zorder"] = 1
+        
+        if agent.color == 1:
+            portrayal["color"] = "#29a918"  # Vert clair
+        elif agent.color == 2:
+            portrayal["color"] = "#ecac0a"  # Jaune clair
+        elif agent.color == 3:
+            portrayal["color"] = "#e41300"  # Rouge clair
+
+    return portrayal
+
+model = RobotModel(**model_params, seed=0)
+
+SpaceGraph = make_space_component(agent_portrayal, post_process=configure_axes)
+
+
+# Create the Dashboard
+page = SolaraViz(
+    model,
+    components=[SpaceGraph],
+    model_params=model_params,
+    name="Waste Collection",
+)
+# This is required to render the visualization in the Jupyter notebook
+page
+# to start : "solara run server.py"
