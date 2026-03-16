9ac84b8e8ca69fc7bb6db8c573b42e21d8fdd256
Nicolas Charrondiere <nicolas.charrondiere@student-cs.fr>
2026-03-16 14:39:19 +0100

    implemented Radioctivity, WasteDisposalZone and Waste Agents

--- objects.py
+++ objects.py
@@ -8,3 +8,42 @@
 #
 # FILE: objects.py
 ################################################################################
+
+from mesa import Agent, Model
+import random as rd
+
+class Radioactivity(Agent):
+    def __init__(self, model:Model, zone:int):
+        """Create a new Racioactivity agent.
+
+        Args:
+            model (Model): The model instance that contains the agent
+        """
+        super().__init__(model)
+
+        self.zone = zone
+        self.radioactivity = ((zone - 1) / 3) + rd.uniform(0, 1/3)
+
+
+class WasteDisposalZone(Radioactivity):
+    def __init__(self, model: Model, zone: int):
+        """Create a new WasteDisposalZone agent.
+
+        Args:
+            model (Model): The model instance that contains the agent
+            zone (int): The zone in wich the agent is (1, 2 or 3)
+        """
+        super().__init__(model, zone)
+
+
+class Waste(Agent):
+    def __init__(self, model: Model, color:int):
+        """Create a new Waste agent.
+
+        Args:
+            model (Model): The model instance that contains the agent
+            color (str): The color of the waste (1:'green', 2:'yellow' or 3:'red')
+        """
+        super().__init__(model)
+        
+        self.color = color
